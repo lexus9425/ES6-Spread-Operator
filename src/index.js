33348507c95b4db11637507c30f904998a57baad
lexus9425 <68475116+lexus9425@users.noreply.github.com>
2022-08-30 14:13:50 -0400

    Change App.jsx
    - Replace if/else statement in setContact with spread operator

--- src/index.js
+++ src/index.js
@@ -3,3 +3,20 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+// Spread Operator Examples
+// const citrus = ["Lime", "Lemon", "Orange"];
+// const fruits = ["Apple", "Banana", "Coconut"];
+
+// const fullName = {
+//   fName: "James",
+//   lName: "Bond"
+// };
+
+// const user = {
+//   ...fullName,
+//   id: 1,
+//   username: "jamesbond007"
+// };
+
+// console.log(user);
